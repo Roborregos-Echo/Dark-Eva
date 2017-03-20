7795bdd4cbc8e4f4c3badb9d2636daa52d4f1670
Osvaldoav <jorgeosvaldoav@gmail.com>
2017-03-20 16:02:38 -0600

    Lcd.prints corregidos

--- src/main.cpp
+++ src/main.cpp
@@ -1299,7 +1299,7 @@ void checarRampa2() {
         {
             if(cuadros[x_last][y_last][z_actual].getEstado() != RECORRIDO)
                 cuadros[x_last2][y_last2][z_actual].setEstado(SIN_RECORRER);
-                
+
             Last2 = false;
         }
 
@@ -2168,6 +2168,7 @@ void Pathfinding(byte x_destino, byte y_destino, byte &ref) {
     ////lcd.println("Actual = " + String(x_actual)+ "," + String(y_actual));
     ////lcd.println("Destino = " + String(x_destino)+ "," + String(y_destino));
     lcd.setCursor(0, 1);
+    lcd.print("                ");
     lcd.print("    " + String(x_destino) + "," + String(y_destino) + "," + String(z_actual));
     bool pathFinished = false;
     int Grid;
@@ -2483,6 +2484,7 @@ void Pathfinding(byte x_destino, byte y_destino, byte &ref) {
                                 }
                             }
                             lcd.setCursor(0, 1);
+                            lcd.print("          ");
                             lcd.print(String(x_actual) + "," + String(y_actual) + "," + String(z_actual));
                         }
                     } else {
